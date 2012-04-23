6877bc43c02483602e2a1eb716505150eea58e77
Eoghan Lappin <eoghanlappin@gmail.com>
2012-04-23 01:35:19 +0100

    Removed print lol from sites.py

--- sites.py
+++ sites.py
@@ -1,5 +1,3 @@
 class Sites:
     irishtimes = { 'main' :
-            'http://www.irishtimes.com/feeds/rss/newspaper/index.rss', 'irish' : 'http://www.irishtimes.com/feeds/rss/breaking/irish.rss', 'world' : 'http://www.irishtimes.com/feeds/rss/breaking/world.rss', 'business' : 'http://www.irishtimes.com/feeds/rss/breaking/business.rss', 'sport' : 'http://www.irishtimes.com/feeds/rss/breaking/sports.rss', 'technology' :'http://www.irishtimes.com/feeds/rss/breaking/technology.rss'}
-
-    print "lol"
+            'http://www.irishtimes.com/feeds/rss/newspaper/index.rss', 'ireland' : 'http://www.irishtimes.com/feeds/rss/breaking/irish.rss', 'world' : 'http://www.irishtimes.com/feeds/rss/breaking/world.rss', 'business' : 'http://www.irishtimes.com/feeds/rss/breaking/business.rss', 'sport' : 'http://www.irishtimes.com/feeds/rss/breaking/sports.rss', 'technology' :'http://www.irishtimes.com/feeds/rss/breaking/technology.rss'}
